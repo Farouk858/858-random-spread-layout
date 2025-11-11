c2bdfa07c78f860282b704b86698ac8be78b937e
Farouk858 <faroukalaofa@gmail.com>
2025-11-11 22:15:49 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/858-random-spread-layout/', // <- must match your repo name
+  base: '/858-random-spread-layout/', // must match your repo name
 })
